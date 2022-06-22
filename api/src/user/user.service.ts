567f5bdc62f0507fd893541cae083c2343fe72e3
Seohyun Kim <kshjessica@ewhain.net>
2022-06-22 13:25:39 +0900

    fix: path changes

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
-import { UserParams } from './user.inference';
+import { UserParams } from './inference/user.inference';
 
 @Injectable()
 export class UserService {
