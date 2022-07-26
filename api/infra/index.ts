c0344171eae81062d43bb7092d01b72b6cf8e093
Seohyun Kim <kshjessica@ewhain.net>
2022-07-26 18:59:22 +0900

    feat: add ecs fargate service

--- api/infra/index.ts
+++ api/infra/index.ts
@@ -1,7 +1,54 @@
-import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
+import * as pulumi from '@pulumi/pulumi';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-bucket');
+const cluster = new awsx.ecs.Cluster('mlops-curriculum-iac');
+const dbListener = new awsx.elasticloadbalancingv2.NetworkListener('db', {
+  port: 5432,
+});
+const serviceListener = new awsx.elasticloadbalancingv2.NetworkListener(
+  'mlops-seohyun-srvc',
+  { port: 3000 },
+);
+const endpoint = dbListener.endpoint;
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const config = new pulumi.Config();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const service = new awsx.ecs.FargateService('mlops-seohyun-srvc', {
+  cluster: cluster,
+  taskDefinitionArgs: {
+    containers: {
+      container: {
+        image:
+          '854407906105.dkr.ecr.ap-northeast-2.amazonaws.com/mlopscurriculum:mlops-seohyun',
+        cpu: 256,
+        memory: 512,
+        portMappings: [serviceListener],
+        environment: endpoint.apply((e) => [
+          {
+            name: 'DB_HOST',
+            value: config.requireSecret('dbHost'),
+          },
+          {
+            name: 'DB_NAME',
+            value: config.requireSecret('dbName'),
+          },
+          {
+            name: 'DB_PASSWORD',
+            value: config.requireSecret('dbPwd'),
+          },
+          {
+            name: 'DB_PORT',
+            value: e.port.toString(),
+          },
+          {
+            name: 'DB_USERNAME',
+            value: config.requireSecret('dbUsr'),
+          },
+        ]),
+      },
+    },
+  },
+});
+
+export const serviceURL = serviceListener.endpoint;
